9c37f0c1eebcb563eb2f801cb19c6490af4c7dbc
Furious_Gamer <102981140+Hubrecht1@users.noreply.github.com>
2025-03-04 22:55:27 +0100

    I did a lot of improvements
    added ai generated status
    added !code command

--- main.py
+++ main.py
@@ -10,22 +10,26 @@ import subprocess
 import os
 from dotenv import dotenv_values
 import string
+import nest_asyncio
+nest_asyncio.apply()
 
 #init:
-greets = ["Hey", "Hello", "Hi", "Yo", "Howdy", "What's up", "Hiya", "Hey there", "Sup", "Greetings", "hoi", "moi", "fakka", "hallo", "goede", "!", "greet", "greetbot"]
-farewells = ["farewell", "bye", "doei", "fuckoff", "go away", "ga weg", "later", "totziens", "farewell", "goodbye", "shutup"]
+greets = ["Hey", "Hello", "Hi", "Yo", "Howdy", "What's up", "Hiya", "Hey there", "Sup", "Greetings", "hoi", "moi", "fakka", "hallo", "goede", "!", "greet", "greetbot", "hoi,"]
+farewells = ["farewell", "bye", "doei", "fuckoff", "go away", "ga weg", "later", "totziens", "farewell", "goodbye", "shutup", "ga", "go"]
 
-AIModel = 'llama3.2'
+ChatModel = 'llama3.2'
 prefix = '!'
+secrets = dotenv_values(".env")
 
 class Client(discord.Client):
   lastUser = ''
-  async def on_ready(self):
-    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print(f'{formatted_time} SUCCESS: Logged on as {self.user}.')
-    await changStatus()
+  prevResponse = ''
+  async def on_ready(self):    
     channel = client.get_channel(895354381921304576)
+    await init(self = self)
     await channel.send("Greet bot is online!")
+    await asyncio.sleep(5)
+    await changStatus()
 
   async def on_message(self,message):
     if message.author == self.user:
@@ -41,48 +45,62 @@ class Client(discord.Client):
       loop = asyncio.get_running_loop()
     except RuntimeError:
      loop = None
-    if loop and loop.is_running():
-      asyncio.create_task(AIChat(message))
+    if loop == True and loop.is_running():
+      task = asyncio.create_task(AIChat(message))
     else:
       asyncio.run(AIChat(message)) # Safe to run normally
+
+async def init(self):
+  formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+  os.system('open /Applications/Ollama.app')
+  print(f'{formatted_time} SUCCESS: Logged on as {self.user}.')
 
 def findWordsInMessage(words, message):
   return (any (normalWord.lower() == word for word in message for normalWord in words))
 
 async def changStatus():
-  newActivity = greets[random.randrange(len(greets)-1)]
-  await client.change_presence(activity = discord.CustomActivity(name = newActivity))
+  #newActivity = greets[random.randrange(len(greets)-1)]
+  randInt = random.randint(1,5)
+  response = asyncio.run(getAIResponse(f"Make up a discord status, max {randInt} words!"))
+  await client.change_presence(activity = discord.CustomActivity(name = response))
 
 async def checkCustomCommands(message, prefix):
   content = message.content.removeprefix(prefix)
   content = content.lower()
   if content == 'restart':
     await message.channel.send('Shutting down...')
+    await client.close()
+    os.system('killall Ollama')
+    asyncio.sleep(3)
     subprocess.run('python3 main.py', shell=True)
-    await client.close()
-
-  if content == 'shutdown':
+  elif content == 'shutdown':
     await message.channel.send('Shutting down...')
+    os.system('killall Ollama')
     await client.close()
-
-  if content == 'change status':
+  elif content == 'change status':
     await changStatus()
     await message.channel.send(f'Setting new status')
-  if content == 'clear':
-    await message.channel.purge(limit = 1)
-  if content == 'reset':
-    client.lastUser = ' '  
-  if content == 'github':
+  elif content == 'clear':
+    await message.channel.purge(limit = 1 + 1)
+  elif content == 'reset':
+    client.lastUser = ''  
+    client.prevResponse = ''
+  elif content == 'github':
     await message.channel.send('https://github.com/Hubrecht1/DiscordBot-2025')
-      
+  elif content == 'code':
+    file = discord.File(fp=secrets['codefilePath'])
+    await message.channel.send(file=file)
 
 async def AIChat(messageInfo):
-  userPrompt = await getUserContext(messageInfo)
+  userPrompt = ''
+  userPrompt += (await getUserContext(messageInfo))
+
   userPrompt += messageInfo.content
+  print(userPrompt)
 
   fullResponse = ''
   stream = chat(
-    model=AIModel,
+    model=ChatModel,
     messages=[{'role': 'user', 'content': userPrompt}],
     stream=True,)
   i = 0
@@ -93,7 +111,7 @@ async def AIChat(messageInfo):
         break
      fullResponse += chunk.message.content
 
-  print(f"DeepSeek: {fullResponse}")
+  print(f"{ChatModel}: {fullResponse}")
   if(i >= 2000):
     fullResponse += '...'
     await messageInfo.channel.send(fullResponse)
@@ -101,13 +119,29 @@ async def AIChat(messageInfo):
   else:
     await messageInfo.channel.send(fullResponse)
 
+  client.prevResponse = fullResponse #update prev response
+
+async def getAIResponse(message):
+  fullResponse = ''
+  stream = chat(
+    model=ChatModel,
+    messages=[{'role': 'user', 'content': message}],
+    stream=True,)
+  for chunk in stream:
+    fullResponse += chunk.message.content
+  return fullResponse
+
 async def getUserContext(message):
+  context = ''
   userPrompt = message.content.lower().split()
   if(findWordsInMessage(farewells, userPrompt)):
-   context = f'{message.author.name} says bye(you should say goodby or somthing): '
+   context = f'\n{message.author.name} says bye: '
    client.lastUser = ''
+   client.prevResponse = ''
   else :
-    context = f'{message.author.name}: '
+    if(client.prevResponse != ''):
+      context += "You: '" + client.prevResponse + "'" #adds previous response
+    context += f'\n{message.author.name} says: '
     client.lastUser = message.author.name
   return context
 
@@ -116,5 +150,4 @@ _intents = discord.Intents.default()
 _intents.message_content = True
 client = Client(intents= _intents)
 
-secrets = dotenv_values(".env")
 client.run(secrets['discordBotToken'])
